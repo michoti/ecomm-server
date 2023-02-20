0df9434042eee0a444c018fec6ea0b4a83c122db
shukeel <triggaillest@gmail.com>
2023-02-20 15:34:57 +0300

    modified

--- react-ui/src/main.jsx
+++ react-ui/src/main.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import 'flowbite';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
--- react-ui/tailwind.config.cjs
+++ react-ui/tailwind.config.cjs
@@ -3,13 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
