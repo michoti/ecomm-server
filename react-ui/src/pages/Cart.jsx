bab92d39a77a6acc74b9eeac7c2009bbdf3848d8
shukeel <triggaillest@gmail.com>
2023-03-10 10:42:08 +0300

    modified

--- react-ui/src/pages/Cart.jsx
+++ react-ui/src/pages/Cart.jsx
@@ -39,7 +39,7 @@ const Cart = () => {
             <svg onClick={() => updateCartItemQuantity(item.product_id, item.quantity > 1 ? item.quantity - 1 : item.quantity )} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
 
-            <input className="mx-2 border text-center w-8" type="text" value={item.quantity} />
+            <input className="mx-2 border text-center w-12" type="text" value={item.quantity} />
 
             <svg onClick={() => updateCartItemQuantity(item.product_id, item.quantity + 1 )} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
               <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
